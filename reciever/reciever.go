cef96621a6e36e88ea96761939e0e8a846d5e86c
dheer309 <dheerm309@gmail.com>
2026-04-25 03:44:40 +0530

    added sending the manifest data alongside file

--- reciever/reciever.go
+++ reciever/reciever.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"boltdrop/chunker"
+	"encoding/binary"
+	"encoding/json"
 	"fmt"
 	"io"
 	"net"
@@ -38,9 +41,43 @@ func handleConnection(conn net.Conn) {
 
 	fmt.Println("New connection from ", conn.RemoteAddr())
 
-	file, err := os.Create("recieved.txt")
+	// defining the header of being 8 bytes long
+	header := make([]byte, 8)
+
+	// reading only the first 8 bytes from the connection
+	_, err := io.ReadFull(conn, header)
+
+	if err != nil {
+		fmt.Println("Cannot fetch header", err)
+		return
+	}
+
+	// fetching the manifest's actual length from the header
+	manifestLength := binary.BigEndian.Uint64(header)
+
+	// reading only the manifest
+	manifestJson := make([]byte, manifestLength)
+	_, err = io.ReadFull(conn, manifestJson)
+
+	if err != nil {
+		fmt.Println("Cannot fetch manifest", err)
+		return
+	}
+
+	// populating that json data into the manifest data structure
+	var manifest chunker.Manifest
+	err = json.Unmarshal(manifestJson, &manifest)
+
+	if err != nil {
+		fmt.Println("Cannot convert into Manifest struct", err)
+		return
+	}
+
+	// send the actual file
+	file, err := os.Create(manifest.Filename)
 	if err != nil {
 		fmt.Println("Error: ", err)
+		return
 	}
 
 	defer file.Close()
--- sender/sender.go
+++ sender/sender.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"boltdrop/chunker"
+	"encoding/binary"
+	"encoding/json"
 	"fmt"
 	"io"
 	"net"
@@ -9,8 +11,14 @@ import (
 )
 
 func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("Enter filename when running the program")
+		return
+	}
+
 	// get file path from user
 	filename := os.Args[1]
+	// open the file
 	file, err := os.Open(filename)
 
 	if err != nil {
@@ -20,13 +28,21 @@ func main() {
 
 	defer file.Close()
 
+	// generate manifest for the target file
 	manifest, err := chunker.GenerateManifest(filename)
+
 	if err != nil {
 		fmt.Println("Error while generating manifest:", err)
 		return
 	}
 
-	fmt.Printf("File: %s, Size: %d, Chunks: %d\n", manifest.Filename, manifest.FileSize, len(manifest.Chunks))
+	// convert the manifest in JSON
+	manifestJson, err := json.Marshal(manifest)
+
+	if err != nil {
+		fmt.Println("Error when converting manifest to JSON:", err)
+		return
+	}
 
 	// try to connect to localhost thru tcp
 	conn, err := net.Dial("tcp", "localhost:8000")
@@ -36,6 +52,19 @@ func main() {
 		return
 	}
 	defer conn.Close()
+
+	// create a header and send the manifest's length through it
+	header := make([]byte, 8) // header is 8 bytes long
+	binary.BigEndian.PutUint64(header, uint64(len(manifestJson)))
+	conn.Write(header)
+
+	// after sending the header, send the manifest json itself
+	n, err := conn.Write(manifestJson)
+
+	if err != nil {
+		fmt.Printf("Error while sending manifest of %d bytes: %v", n, err)
+		return
+	}
 
 	// copy file to connection
 	io.Copy(conn, file)
